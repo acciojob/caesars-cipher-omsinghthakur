cc536692172da182f8818c10d16eb2150fc61bbe
Om Singh <94895043+omsinghthakur@users.noreply.github.com>
2023-05-13 22:03:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,18 +4,28 @@ const lookup = {
   'A': 'N','B': 'O','C': 'P','D': 'Q',
   'E': 'R','F': 'S','G': 'T','H': 'U',
   'I': 'V','J': 'W','K': 'X','L': 'Y',
-  'M': 'Z','N': 'A','O': 'B','P': 'C',
+  'M': 'Z','N': 'A','O': 'B','P': 'C', 
   'Q': 'D','R': 'E','S': 'F','T': 'G',
   'U': 'H','V': 'I','W': 'J','X': 'K',
   'Y': 'L','Z': 'M', '?': '?', ',': ','
-};
+}; 
 
 function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-
-  return ;//return decodedArr
-}
+   let decodedArr = []; 
+	var decodedStr = "";
+  
+  for (var i = 0; i < str.length; i++) {
+    var charCode = str.charCodeAt(i);
+    
+    if (charCode >= 65 && charCode <= 90) {  // Check if it's an uppercase letter
+      charCode = ((charCode - 65 + 13) % 26) + 65;  // Apply ROT13 formula
+    }
+    
+    decodedStr += String.fromCharCode(charCode);  // Convert back to a string and append to decoded string
+  }
+  
+  return decodedStr;
+} 
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
